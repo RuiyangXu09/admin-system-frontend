023ea5297049571f217e1f7daa9657b9db6a9037
RuiyangXu09 <332971895@qq.com>
2023-03-22 15:08:05 +1300

    frontend

---
+++ src/api/index.js
@@ -0,0 +1,36 @@
+import request from "../utils/request";
+
+/**
+ * login api
+ */
+export const getLogin = (data) =>{
+    return request({method: 'post', url: '/api/v1/admin/login', data})
+};
+
+/**
+ * register api
+ */
+export const getRegister = (data) =>{
+    return request({method: 'post', url: '/api/v1/admin/register', data})
+};
+
+/**
+ * 获取members列表数据的api
+ */
+export const getMember = (data) =>{
+    return request({method: 'get', url: '/api/v1/member/find', data})
+}
+
+/**
+ * member info修改的api
+ */
+export const changeMemberInfo = (data) =>{
+    return request({method: 'get', url: '/api/v1/member/update', data})
+}
+
+/**
+ * delete api
+ */
+export const deleteMember = (data) =>{
+    return request({method: 'get', url: '/api/v1/member/delete', data})
+}
